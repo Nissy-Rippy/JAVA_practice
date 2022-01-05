f320c2530367b7b2cfd4aa04563899455494cdee
Toyoda Yoshiteru <6089more@gmail.com>
2022-01-05 23:50:28 +0900

    [Java sum]

--- aaa/chap04/Break.java
+++ aaa/chap04/Break.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 class Break {
   public static void main(String[] args){
     Scanner stdIn = new Scanner(System.in);
-    System.out.println("");
+    System.out.println("整数を足していくよー");
+    System.out.print("なにか致していく？？   ");
+    int n = stdIn.nextInt();
+    int sum = 0;
+    int num;
+    for (int i = 0; i <= n; i++){
+      System.out.print("足す数は？");
+      num = stdIn.nextInt();
+      sum += num;
+    }
+    System.out.println("合計は" + sum +"ですよ！");
   }
 }
