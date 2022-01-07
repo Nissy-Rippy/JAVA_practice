b9d99398e554e6b5ed3181d6e05d5382e88d09cb
Toyoda Yoshiteru <6089more@gmail.com>
2022-01-07 23:07:49 +0900

    [java]演算子

--- aaa/chap05/Start.java
+++ aaa/chap05/Start.java
@@ -1,5 +1,23 @@
 package aaa.chap05;
+import java.util.Scanner;
 
 class Start {
-  
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    int a = 12;
+    int b = 012;
+    int c = 0x12;
+    int d = 0X12;
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+    System.out.println("d = " + d);
+
+    System.out.print("整数： ");
+    int num = stdIn.nextInt();
+    System.out.printf("10新数では%dです\n", num);
+    System.out.printf("8新数では%oです\n", num);
+    System.out.printf("16進数では%xです\n", num);
+  }
 }
