1aef91dc8af44b3e9a6f48b414145717dba3b186
Toyoda Yoshiteru <6089more@gmail.com>
2022-01-14 23:58:22 +0900

    [java]

--- aaa/chap06/Array.java
+++ aaa/chap06/Array.java
@@ -1,11 +1,10 @@
 package aaa.chap06;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Array {
-  public static void main(String[] main){
-    int[] a = new int[5];
-    a[1] = 37;
-    a[2] = 51;
-    a[4] = a[1] * 2;
-    
-    }
-}
+  public static void main(String[] mai){
+   Scanner stdIn ==
+   
+  }}
