e8c68cf7f80ef4fd9b8eb1418c7a21300f6a6536
Toyoda Yoshiteru <6089more@gmail.com>
2022-01-21 21:46:15 +0900

    [java]

--- aaa/chap06/Array.java
+++ aaa/chap06/Array.java
@@ -2,10 +2,16 @@ package aaa.chap06;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 
 class Array {
-  public static void main(String[] mai){
-   Scanner stdIn = new Scanner(System.in);
-   
-   
-  }}
+  public static void main(String[] mai) {
+    Scanner stdIn = new Scanner(System.in);
+    Random rand = new Random();
+
+    System.out.print("明日の食べたいご飯はなんですか？:　");
+    String menu = stdIn.next();
+    String[] menus = { "apple", "banana", "budou" };
+
+  }
+}
