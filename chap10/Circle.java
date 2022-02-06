8fd3297d6f6f5045fe67399e7de521dc8949af4d
Toyoda Yoshiteru <6089more@gmail.com>
2022-02-06 23:43:54 +0900

    []java

---
+++ chap10/Circle.java
@@ -0,0 +1,23 @@
+package chap10;
+
+public class Circle {
+  private Point2D center;
+  private int radius = 0;
+
+  public Circle() { center = new Point2D(); }
+  public Circle(Point2D c, int radius) {
+    this.radius = radius; center = new Point2D(c);
+  }
+
+  public Point2D getCenter() { return new Point2D(center); };
+  public int getRadius() { return this.radius; }
+  
+  public void setCenter(Point2D c) {
+    center.set(c.getX(), c.getY());
+  }
+  public void setRadius(int radius) { this.radius = radius; }
+
+  public String toString() {
+    return "中心座標" + center.toString() + "半径" + radius;
+  }
+}
