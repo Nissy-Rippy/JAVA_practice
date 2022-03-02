73cb0407055ca86ce4e5c276b6bd477e52f2871b
Toyoda Yoshiteru <6089more@gmail.com>
2022-03-02 17:57:56 +0900

    [java]

---
+++ chap17/DateApp.java
@@ -0,0 +1,18 @@
+package chap17;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class DateApp {
+  public static void main(String[] args) {
+    Date date = new Date();
+    System.out.println(date.toString());
+    String yyyy = String.format("%tY", date); System.out.println("year = " + yyyy);
+    String MM = String.format("%tm", date);   System.out.println("month = " + MM);
+    String dd = String.format("%td", date);   System.out.println("day = " + dd);
+    String HH = String.format("%tH", date);   System.out.println("hour = " + HH);
+    String m = String.format("%tM", date);    System.out.println("minute = " + m);
+    String s = String.format("%tS", date);    System.out.println("second = " + s);
+    SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:m:s ");
+    System.out.println("SimpleDateFormat: " + df.format(date));
+  }
+}
---
+++ chap17/File_ijnput2.java
@@ -0,0 +1,29 @@
+package chap17;
+import java.io.*;
+
+public class File_ijnput2 {
+  public static void main(String[] args) {
+    int MAX = 20;
+    int[] rand_number = new int[MAX];
+    try{
+      File file = new File("rand_20F.txt");
+      BufferedReader br = new BufferedReader(new FileReader(file));
+      int row = 0;
+      String s;
+      while ( (s = br.readLine()) != null) {
+        String[] str = s.split(",");
+        int col_MaX = str.length;
+        for (int i = 0; i < col_MaX; i++) {
+          rand_number[row * col_MaX + i] = Integer.parseInt( str[i].trim( ) );
+        }
+        row++;
+      }
+      br.close();
+    } catch (IOException e) { System.out.println(e); }
+
+    for (int i = 0; i < MAX; i++){
+      if(i % 5 == 0 && i != 0) { System.out.println(); }
+      System.out.printf("%5d,", rand_number[i]);
+    }
+  }
+}
