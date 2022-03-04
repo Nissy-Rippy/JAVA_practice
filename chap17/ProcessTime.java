d405f108eea1e8a250074e9ebcfbfe2550bddbfa
Toyoda Yoshiteru <6089more@gmail.com>
2022-03-04 11:03:15 +0900

    [java]

---
+++ chap17/ProcessTime.java
@@ -0,0 +1,21 @@
+package chap17;
+
+public class ProcessTime {
+  public static void main(String[] args) {
+    long start = System.nanoTime();
+    for (int i = 0; i < 1000; i++) { ;}
+    long end = System.nanoTime();
+    System.out.println("for loop     : " + (end -start) + "(nsec)");
+    
+    start = System.nanoTime();
+    int i = 0; while(i < 10000) { ; i++; }
+    end = System.nanoTime();
+    System.out.println("while loop  :" + (end -start) + "(nsec)");
+
+    start = System.nanoTime();
+    i = 0; do { ; i++; } while(i < 1000);
+    end = System.nanoTime();
+    System.out.println("do-while    : " + (end -start) + "(nsec)");
+
+  }
+}
