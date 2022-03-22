90130b63be5a196a8a3643adc30551ed9d6df694
Toyoda Yoshiteru <6089more@gmail.com>
2022-03-22 22:52:33 +0900

    [java]

--- chap19/Practice.java
+++ chap19/Practice.java
@@ -35,9 +35,7 @@ public class Practice {
     // (Integer x, Integer y) -> { return Integer.compare(x, y); }
     // );
     System.out.println(list3);
+    System.out.println(list3);
 
-
-
-    
   }
 }
