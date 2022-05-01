00e6721ace520079d3ea3ec11cef0920c88cc0d6
Toyoda Yoshiteru <6089more@gmail.com>
2022-05-01 17:30:27 +0900

    [java]

--- chap19/Interface.java
+++ chap19/Interface.java
@@ -5,7 +5,6 @@ public interface Interface {
   public abstract void stop();
   public static void keep(){ System.out.println("アレクサ"); }
 }
-
 // @FunctionalInterface2つあるので、エラーになる！
 interface Interface2 {
   public abstract void stop();
