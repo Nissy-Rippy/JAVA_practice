06e3fd59143d547e21b82cd31b618e39cef03e9a
Toyoda Yoshiteru <6089more@gmail.com>
2022-06-20 14:26:24 +0900

    [null の安全性について！]

--- chap22/Pocket.java
+++ chap22/Pocket.java
@@ -1,6 +1,6 @@
 package chap22;
 
-
+import java.util.Optional;
 
 //<>ジェネリックスで指定しないと、コンパイル時にエラーに気づけなくなる
 // public class Pocket {
@@ -35,6 +35,32 @@ package chap22;
 public class Pocket<E extends NoType> {
 
   public void put(NoType noType) {
+
   }
+//Optionalクラスは、1つのインスタンスを格納するだけのクラス  
+  public static void main(String[] args) {
+    Optional<String> op1 = Optional.ofNullable("いなと");
+    Optional<Integer> op3 = Optional.ofNullable(2);
+    Optional<String> op4 = Optional.ofNullable(null);
+        //Optional.of();はnullを許さないメソッドである
+    // Optional<String> op2 = Optional.of(null);
+
+    System.out.println(op1);
+    // System.out.println(op2);
+
+    //中身があるかどうか確認する
+    System.out.println(op1.isPresent());
+
+    System.out.println("勇者" + op1.get() );
+
+    //get()の際にnullだとエラーが発生する
+    // System.out.println("勇者" + op4.get());
+
+    //orElseはnullを置換して内容を取得できる。
+    System.out.println("勇者" + op1.orElse("ななし") );//いなと　のまま
+    System.out.println("勇者" + op4.orElse("ななし") );//nullなので　ななしに入れ替わる
+
+
+    }
   
 }
---
+++ chpa23/Optional1.java
@@ -0,0 +1,27 @@
+package chpa23;
+
+
+
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.OptionalDouble;
+
+
+public class Optional1 {
+
+  public static Optional<String> decorate(String s, char c) {
+    String r;
+    if( s == null || s.length() == 0) { r = null; }
+    else {  r = c + s + c; }
+    return Optional.ofNullable(r);
+  }
+
+  public static void main(String[] args) {
+    Optional<String> sc = decorate("as", '*');
+    System.out.println(sc);
+
+    Optional<String> sf = decorate(null, 'x');
+
+    System.out.println(sf.orElse("nullのためこういう結果になりました！"));
+  }
+}
