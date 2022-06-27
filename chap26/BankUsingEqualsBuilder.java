2e798d82f86b896c932deae5758251cb58f38cd5
Toyoda Yoshiteru <6089more@gmail.com>
2022-06-28 05:50:08 +0900

    [この章の復習そして、様々なライセンスについての理解]

--- chap26/BankUsingEqualsBuilder.java
+++ chap26/BankUsingEqualsBuilder.java
@@ -2,10 +2,12 @@ package chap26;
 
 import org.apache.commons.lang3.builder.*;
 
-public class BankUsingEqualsBuilder {
+public class BankUsingEqualsBuilder implements Cloneable, Comparable<BankUsingEqualsBuilder>{
+//フィールド
+  String title;
+  String comment;
+  String publishedDate;
 
-  String name;
-  String address;
 
   public boolean equals(Object o) {
     //reflectionsEquals はBankクラスが持つ全てのフィールドが等価であることを自動的に調べてくれるのである！
@@ -16,5 +18,11 @@ public class BankUsingEqualsBuilder {
   public int hashCode() {
     return HashCodeBuilder.reflectionHashCode(this);//Bankクラスの全フィールドから、ハッシュ値を生成できるようになるらしい！/*  */
   }
-  
+
+
+  public int compareTo(BankUsingEqualsBuilder o) {
+    return CompareToBuilder.reflectionCompare(
+      this, o, "comment", "title"
+    );
+  }
 }
