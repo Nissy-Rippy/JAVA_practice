26a77943b8ca12ea49cd20626922a851822d2798
Toyoda Yoshiteru <6089more@gmail.com>
2022-06-28 16:14:28 +0900

    [第二章　復讐完了]

--- review/Review2.java
+++ review/Review2.java
@@ -1,5 +1,35 @@
 package review;
 
-public class Review2 {
-  
+
+public class Review2<E> {
+
+  private E e;
+  private E keyType;
+  private long count;
+
+  public Review2(KeyType keyType) { this.keyType = keyType; }
+
+  public void put(E e)  {
+    this.e = e;
+  }
+
+  public E get() {
+    this.count++;
+    switch (this.keyType) {
+      case PADLOCK:
+      if (count < 1024) return null;
+      break;
+      case BUTTON:
+      if (count < 10000) return null;
+      break;
+      case DIAL:
+      if (count < 30000) return null;
+      break;
+      case FINGER:
+      if (count < 1000000) { return null; }
+      this.count = 0;
+      return this.keyType;
+  }
+}
+
 }
