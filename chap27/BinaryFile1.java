b477958e7608a7a117f925df1bf1ddb36df920a3
Toyoda Yoshiteru <6089more@gmail.com>
2022-06-30 13:31:28 +0900

    [java 6-5ファイルの操作の落とし穴　終了！]

--- chap27/BinaryFile1.java
+++ chap27/BinaryFile1.java
@@ -5,13 +5,18 @@ import java.io.FileOutputStream;
 import java.io.FileInputStream;
  public class BinaryFile1 {
   public static void main(String[] args) throws IOException {
-    FileOutputStream fos = 
-    new FileOutputStream("rpgsave.dat", true);
 
+    try {
+    FileOutputStream fos =
+    new FileOutputStream("rpgsave.dat", true);
     fos.write(65);//65は2進数で01000001 = Aを書き込んでることと同じである
     fos.flush();
+    } catch (IOException e) {
+       System.out.print(e);
+      }
+    finally {
     fos.cloes();
-
+    }
 
 
   }
--- chap27/Filewr.java
+++ chap27/Filewr.java
@@ -7,13 +7,29 @@ import java.io.FileReader;
 
 public class Filewr {
   public static void main(String[] args) throws IOException {
+    FileWriter fw = null;
+    try{
     // true はデータの末尾からデータを追記していくfalse、省略の場合はファイルの先頭からデータを上書きしていきます
     FileWriter fw = new FileWriter("rpgsave.dat", true); // 第一引数は、ファイル名！！ファイルがない場合は新しく作成する！
     // flush()をファイルを閉じる前にしないと正しくデータが書き込まれないことがある。そのため、JVMに今すぐカキコでない部分を書き出せとの支持
     fw.write("A");
-    fw.flush();
-    fw.close();
+    fw.flush(); }
+    catch(IOException e) { System.out.print("エラーが起きました！"); }
+    finally{
+      if(fw != null) {
+       try{
+         fw.close();
+      } catch (IOException e) { System.out.println("ファイル閉じるのに失敗！");}
+    }
+  }
 
-   
+  //try-With-resoureces文で書くとこうなる！
+// try(){}
+  try(
+    FileWriter ff = new FileWriter("xbox", true);//自動的にclose を呼び出してくれるのでfinally文を書かずにスマートにかける！
+  ) { ff.write();
+      ff.flush();
+    } catch (IOException e) { throw e; }
+
   }
 }
