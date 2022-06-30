6300086106afd4f4259a94dee11d1dcb483523bd
Toyoda Yoshiteru <6089more@gmail.com>
2022-06-30 13:15:52 +0900

    [java 6-3,6-4開始,6-4 終了！ 6-5ファイル操作の落とし穴　開始！]

--- chap27/BinaryFile1.java
+++ chap27/BinaryFile1.java
@@ -1,6 +1,18 @@
 package chap27;
 
-
+import java.io.*;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
  public class BinaryFile1 {
-  
+  public static void main(String[] args) throws IOException {
+    FileOutputStream fos = 
+    new FileOutputStream("rpgsave.dat", true);
+
+    fos.write(65);//65は2進数で01000001 = Aを書き込んでることと同じである
+    fos.flush();
+    fos.cloes();
+
+
+
+  }
 }
