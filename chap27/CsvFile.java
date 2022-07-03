57dea7cea24c03901ae5b041e3f000ce35006647
Toyoda Yoshiteru <6089more@gmail.com>
2022-07-03 10:58:25 +0900

    [java 7-2 CSV形式 finish ＊ jarファイルが参照できていない！]

--- chap27/CsvFile.java
+++ chap27/CsvFile.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public class CsvFile {
   public static void main(String[] args) {
-    
+    String ss = "田中、佐藤、渡辺、小林、大池";
+    String[] s = ss.split(",");
+
+    for(String t : s) {
+      System.out.println(t);
+    }
   }
 }
