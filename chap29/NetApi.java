88e3b14f3d2755183b1d72d450a254feda2a8bd1
Toyoda Yoshiteru <6089more@gmail.com>
2022-07-07 11:08:17 +0900

    [java 8-1 ネットワークAPIの全体像 finish ]

--- chap29/NetApi.java
+++ chap29/NetApi.java
@@ -1,11 +1,19 @@
-import java.net.http.*;
-import java.net.spi.*;
-
+import java.net.*;
+import java.io.*;
 
 public class NetApi {
-  public static void main(String[] args) {
-     URL URL1 = new URL("https://dokojava.jp");//引数一つだとＳtring型のコンストラクタが用意されている。
-     InputStream is = url.openStream();//InputStream型で返す
-     int data = is.read();//read()を呼び出して。ストリームからⅠバイトずつ情報を取り出す。
+  public static void main(String[] args) throws IOException, Exception {
+
+    URL url = new URL("https://dokojava.jp");// 引数一つだとString型のコンストラクタが用意されている。
+    InputStream is = url.openStream();// InputStream型で返す
+    InputStreamReader isr = new InputStreamReader(is);//
+
+    int data = isr.read();// read()を呼び出して。ストリームからⅠバイトずつ情報を取り出す。初めの一文字を代入しておく
+    while (data != -1) {
+      System.out.print((char) data);// print で横一列に表示！！！char型にキャスティﾝグして文字列に変換、する -1はcharで表せないためデータ型で受け取るしかないのである。
+      data = isr.read();// そしてまた、新しい情報を代入する
+    }
+    isr.close();// 読み込むんだらしっかり最後は閉じる。
+
   }
 }
