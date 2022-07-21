084b5b0bb30802deff18d2182d6fe8a86fcc8783
Toyoda Yoshiteru <6089more@gmail.com>
2022-07-21 17:52:27 +0900

    [java 10-3 バッチとAnt　ビルドツール　start]

--- javaWcp/src/Test11.java
+++ javaWcp/src/Test11.java
@@ -1,5 +1,6 @@
 package javaWcp.src;
 
+import java.nio.channels.IllegalSelectorException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,21 +9,26 @@ import java.sql.ResultSet;
 
 
 public class Test11 {
+
+    // SQliteのＤriverの名前
     private static final String DRIVER = "com.mysql.jdbc.Driver";
+    // 扱うデータベースの種類、 テーブル名とユーザー名とパスワードとＳＳＬ送信
     private static final String URL = "jdbc:mysql://localhost:3306/database01?user=user01&password=password01&useSSL=false";
 
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, Exception {
 
         try {
-            Class.forName(DRIVER);
+            Class.forName(DRIVER);//完全限定名
         } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("ドライバー接続に失敗したよ！");
             e.printStackTrace();
             throw e;
         }
 
         // Connection（データベースとの接続を表す）、PreparedStatement（発行するSQLを表す）を、それぞれ生成します。
         try (Connection connection = DriverManager.getConnection(URL);
+                      //ひな形の作成
                 PreparedStatement statement = connection.prepareStatement("select * from user01");
 								// PreparedStatement statement1 = connection.prepareStatement("select * from user01 where id = ?");
                                 // PreparedStatement statement2 = connection.prepareStatement("insert into user01 (email, name) values (?,?)");
