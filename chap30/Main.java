d206ec5d486312c8dc89391bc756d87bdd379596
Toyoda Yoshiteru <6089more@gmail.com>
2022-07-24 05:37:33 +0900

    [java 10章 mavenの利用方法とかき方！start&finish (環境変数のやり方が何故かおかしい・復習]

---
+++ chap30/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) throws Exception {
+     System.out.println("ｽｯｷﾘ魔王征伐　ver1.0.0 by minato");
+     System.out.println("Mavenで鋭意開発中！マテ次号！！");
+  }
+}
