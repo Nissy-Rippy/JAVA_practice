b2585da93130fb6200852563e89af6381d9a51fd
Toyoda Yoshiteru <6089more@gmail.com>
2022-07-25 17:36:08 +0900

    [java 10章 javadocの復習 start]

---
+++ chap30/javadocTest.java
@@ -0,0 +1,12 @@
+
+package jp.miyabilink.atm;
+
+public class javadocTest {
+  String name;
+  String address;
+
+  public void addAccount(String owner, int initZandaka) { }
+  public static void main(String[] args){
+    System.out.println("試験用のメインメソッドです！");
+  }
+}
