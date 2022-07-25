cb944099b7768c9d84d2a622fa0771910eb5b4c1
Toyoda Yoshiteru <6089more@gmail.com>
2022-07-25 17:53:21 +0900

    [java １０章　javadoc 練習途中]

--- chap30/javadocTest.java
+++ chap30/javadocTest.java
@@ -1,10 +1,19 @@
-
+/**
+ * jp.miyabilink.atmのパッケージです！
+ */
 package jp.miyabilink.atm;
-
+/**
+ * これは、javadocを書く練習クラスです！
+ */
 public class javadocTest {
+  /**   銀行の名前   */
   String name;
+  /** 銀行の住所 */
   String address;
-
+/** 
+ * 口座にお金を加えるメソッド
+ * @param owner 銀行名義人
+ */
   public void addAccount(String owner, int initZandaka) { }
   public static void main(String[] args){
     System.out.println("試験用のメインメソッドです！");
