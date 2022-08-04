ab6bc43f0a9a10e2fcd13af36b1690ea789281e6
Toyoda Yoshiteru <6089more@gmail.com>
2022-08-04 23:59:12 +0900

    [java 4~6章の復習 途中]

--- review4~6/Jvm.java
+++ review4~6/Jvm.java
@@ -1,3 +1,35 @@
+import java.util.Properties;
+
 public class Jvm {
-  
+  final String BR = System.getProperty("line.separator");
+  public static void main(String[] args) throws Exception {
+    System.out.println("計算完了。結果をメモ帳で表示します！！");
+    // ProcessBuilder pb = new ProcessBuilder("c:\\windows\\system\\notepad.exe", "calcreport.txt");
+    // pb.start();
+
+    System.out.println("利用中のバージョン");
+    System.out.println(System.getProperty("java.version"));
+    System.out.println(System.getProperty("java.home"));
+
+    Properties p = System.getProperties();
+    Iterator<String> i = p.stringPropertyNames().iterator();
+    System.out.println("システムピロパティー一覧");
+    while(i.hasNext()) {
+      String key = i.next();
+      String.print( key + " = " );
+      System.out.println(System.getProperty(key));
+    }
+
+    System.setProperty("rpg.version", "0.3");
+    System.setProperty("rpg.author", "yoshiteru");
+    String ver = System.getProperty("rpg.version");
+    String au = System.getProperty("rpg.author");
+
+    System.out.println("RPG: スッキリ魔王征伐 ver " + ver );
+    boolean err = false;
+
+    if (err == true) {
+      System.exit(0);
+    }
+  }
 }
