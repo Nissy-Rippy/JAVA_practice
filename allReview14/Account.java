ff4e99334882ea4168a1780980d84b6b1ceff45c
Toyoda Yoshiteru <6089more@gmail.com>
2022-08-18 16:29:17 +0900

    [Finish] スッキリわかるJAVA入門　実践編 14章(ほぼテキストの読解のみ)

---
+++ allReview14/Account.java
@@ -0,0 +1,13 @@
+public class Account {
+  private int zandaka;
+
+  public int getZandaka() { return this.zandaka; }
+
+  public void setZandaka(int zandaka) { 
+    if (x < 0) { throw new IllegalArgumentException("ふの残高たの設定がされそうになりました！"); }
+    this.zandaka = zandaka; 
+  }
+     
+
+
+}
